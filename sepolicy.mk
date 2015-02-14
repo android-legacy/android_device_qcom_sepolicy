0ba7269c2cb380e7c381289c9e35111acfbab66c
erikcas <erikcas1972@gmail.com>
2015-02-14 21:05:16 +0100

    underp
    Change-Id: I49a26e0e3ee3bd74336bcd83403326eaf05fd6fa

--- sepolicy.mk
+++ sepolicy.mk
@@ -3,7 +3,7 @@ BOARD_SEPOLICY_DIRS += \
        device/qcom/sepolicy \
        device/qcom/sepolicy/common \
        device/qcom/sepolicy/test \
-       device/qcom/sepolicy/$(TARGET_BOARD_PLATFORM)
+       device/qcom/sepolicy/$(TARGET_BOARD_PLATFORM) \
        device/qcom/sepolicy/sony
 
 BOARD_SEPOLICY_UNION += \
