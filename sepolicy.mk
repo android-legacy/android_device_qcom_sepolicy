AOSPA/android_device_sony_sepolicy

Sony: Add SELinux policy
Dear init,
Most of the services which needed a SELinux domain
now have one, and the denials have been fixed :)

* Add domains for sony services
* Fix sensors' access to tad socket

sepolicy.mk
@@ -3,7 +3,8 @@ BOARD_SEPOLICY_DIRS += \
        device/sony/sepolicy \
        device/sony/sepolicy/common \
        device/sony/sepolicy/test \
-       device/sony/sepolicy/$(TARGET_BOARD_PLATFORM)
+       device/sony/sepolicy/$(TARGET_BOARD_PLATFORM) \
+       device/sony/sepolicy/sony
 
 BOARD_SEPOLICY_UNION += \
        genfs_contexts \
@@ -91,6 +92,12 @@ BOARD_SEPOLICY_UNION += \
        hbtp.te \
        kernel.te \
        vold.te
+
+BOARD_SEPOLICY_UNION += \
+       sct.te \
+       ta_qmi.te \
+       tad.te \
+       thermanager.te
 
 # Compile sensor pilicy only for SSC targets
 SSC_TARGET_LIST := apq8084

android-legacy/android_device_qcom_sepolicy

Change-Id: Ifc903a19ea10a1528534eec58fffa008521d4500

sepolicy.mk
@@ -4,6 +4,7 @@ BOARD_SEPOLICY_DIRS += \
        device/qcom/sepolicy/common \
        device/qcom/sepolicy/test \
        device/qcom/sepolicy/$(TARGET_BOARD_PLATFORM)
+       device/qcom/sepolicy/sony
 
 BOARD_SEPOLICY_UNION += \
        genfs_contexts \
@@ -91,6 +92,11 @@ BOARD_SEPOLICY_UNION += \
        vold.te
 
 -include device/qcom/sepolicy/$(TARGET_BOARD_PLATFORM)/Android.mk
+BOARD_SEPOLICY_UNION += \
+       sct.te \
+       ta_qmi.te \
+       tad.te \
+       thermanager.te
 
 # Compile sensor pilicy only for SSC targets
 SSC_TARGET_LIST := apq8084